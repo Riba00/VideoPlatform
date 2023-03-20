8db0b6e5ef1d1f53c9ff55b635236528619a0b82
Riba00 <jordiriba@iesebre.com>
2023-03-20 17:55:42 +0100

    WIP

--- app/helpers.php
+++ app/helpers.php
@@ -57,7 +57,7 @@ if (!function_exists('create_superadmin_user')) {
     {
         $user = User::create([
             'name' => 'SuperAdmin',
-            'email' => 'superadmin@casteaching.com',
+            'email' => 'jordi.riba.ferre@gmail.com',
             'password' => Hash::make('12345678')
         ]);
         $user->superadmin = true;
